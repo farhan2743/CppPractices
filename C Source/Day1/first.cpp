5146f9bbb377f0377aea2b5ed4b11dafa443a7e5
farhan2743 <hfarhan2743@gmail.com>
2022-10-16 19:07:37 +0600

    input

--- C Source/Day1/first.cpp
+++ C Source/Day1/first.cpp
@@ -7,7 +7,7 @@ Day one program
 */
 
 #include<stdio.h>   //linking sec
-
+// stdio = standard i/o
 
 #define pi 3.1416  //define sec
 
@@ -16,6 +16,8 @@ double rediou;         // Global Diclaration sec
 
 int main(){             // Main Function
     double area;
+    printf("Enter Value of redious: ");
+    scanf("%lf",&rediou);
     area=pi*rediou*rediou;
     printf("radius is : %lf Area is : %lf",rediou,area);
 }
