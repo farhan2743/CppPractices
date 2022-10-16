1fad6c14d7b4fe158fad2a74e660db3a81b983af
farhan2743 <hfarhan2743@gmail.com>
2022-10-16 18:34:27 +0600

    2nd

---
+++ C Source/Day1/first.cpp
@@ -0,0 +1,18 @@
+/*
+Doc sec
+
+Day one program
+ Athur by farhan
+ this is my first Program
+*/
+
+#include<stdio.h>   //linking sec
+
+#define pi 3.1416;  //define sec
+
+int i=1000;      
+
+
+int main(){
+    printf("hello world");
+}
