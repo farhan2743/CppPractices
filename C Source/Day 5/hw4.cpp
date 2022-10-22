8ee57717d2349850612e0f87139bed29b6e75c7c
farhan2743 <hfarhan2743@gmail.com>
2022-10-23 01:53:45 +0600

    hw

---
+++ C Source/Day 5/hw4.cpp
@@ -0,0 +1,27 @@
+/*Write a C program to check whether a number is divisible by 5 and 11 or not using if else.
+ How to check divisibility of any number in C programming. C program to enter any number and 
+ check whether it is divisible by 5 and 11 or not.
+
+Input
+Input number: 55
+
+Output
+Number is divisible by 5 and 11*/
+#include<stdio.h>
+int main()
+{
+    int number;
+    printf("Enter any number :");
+    scanf("%d",&number);
+    if (number%5==0)
+    {  if (number%11==0)
+    {
+    printf("The number is  divisible by 5 and 11");
+      }
+    
+       else  printf("The number is  not divisible by 5 and 11");
+    }
+    else 
+    printf("The number is not divisible by 5 and 11 ");
+    return 0;
+}
