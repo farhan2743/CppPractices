4492b322381d8667fad5454697ed5acf05e842b3
farhan2743 <hfarhan2743@gmail.com>
2022-10-23 02:36:32 +0600

    oct23

---
+++ C Source/Day 5/hw7.cpp
@@ -0,0 +1,22 @@
+/*Write a C program to input cost price and selling price of a product and check profit or loss.
+ Also calculate total profit or loss using if else.
+
+Input
+Input cost price: 1000 
+Input selling price: 1500
+
+Output
+Profit: 500*/
+
+#include<stdio.h>
+int main(){
+    int a,b,c;
+    printf("Input cost price:");
+    scanf("%d",&a);
+    printf("Input selling price:");
+    scanf("%d",&b);
+    c=b-a;
+    printf("Profit: %d",c);
+    return 0 ;
+
+}
