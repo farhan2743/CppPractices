f8f9a8c4a91e5517e3a905a0575814971e8bd5f4
farhan2743 <hfarhan2743@gmail.com>
2022-10-25 18:59:35 +0600

    practice1Done

---
+++ C Source/Day  7/practice1.cpp
@@ -0,0 +1,27 @@
+/*Write a C program to input electricity unit charge and calculate the total electricity bill according
+to the given condition:
+For first 50 units Rs. 0.50/unit
+For next 100 units Rs. 0.75/unit
+For next 100 units Rs. 1.20/unit
+For unit above 250 Rs. 1.50/unit
+An additional surcharge of 20% is added to the bill.*/
+
+#include<stdio.h>
+int main (){
+    printf("Enter electricity unit charge :");
+    double unit, amount;
+    scanf("%lf",&unit);
+
+    if (unit<=50)
+        amount = unit * .5;
+    else if (unit>50 && unit<=150)
+        amount = (50* 0.5)+(unit-50)*0.75;
+    else if(unit>150 && unit<=250)
+        amount=(50 * 0.5) +(100* 0.75)+(unit-150)*1.20;
+    else
+        amount=(50 * 0.5) +(100* 0.75)+(100 *1.20)+(unit-250)*1.5;
+    amount= amount+ amount*0.2;
+    printf("Tatal bill : %.2lf\n",amount);
+    
+    return 0;
+}
