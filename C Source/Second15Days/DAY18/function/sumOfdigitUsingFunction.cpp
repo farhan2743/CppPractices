996d277e7ce0b9b1d5cb7bd5a74d83503d24c6c5
farhan2743 <hfarhan2743@gmail.com>
2022-11-12 18:15:04 +0600

    fun

---
+++ C Source/Second15Days/DAY18/function/sumOfdigitUsingFunction.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int sumOfDigit(int n);  /// Function prototype
+int main(int argc, char const *argv[])
+{
+    int n,a,i;
+    printf("enter any number :");
+    scanf("%d",&n);
+   
+    while (n>9)
+         n=sumOfDigit(n);  // Function Call 
+    printf("Sum of the digits is : %d",n);
+    return 0;
+}
+
+// Function defination
+int sumOfDigit(int num){
+    int ans=0;
+    while (num)
+    {
+       ans = ans +num%10;
+       num/=10;
+    }
+    return ans;
+}
