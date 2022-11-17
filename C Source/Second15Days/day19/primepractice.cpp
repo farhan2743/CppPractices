011f188d70706796de7cc79de948ede5fcc5934b
farhan2743 <hfarhan2743@gmail.com>
2022-11-17 22:26:00 +0600

    ff

---
+++ C Source/Second15Days/day19/primepractice.cpp
@@ -0,0 +1,17 @@
+//Write a C progream t find prime numbers between 1 to 'n' 
+
+
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int n , i ; 
+    printf("Enter range (1 to n) : ");
+    scanf("%d",&n);
+    for ( i = 1; i <= n ; i++)
+    {
+        
+    }
+    
+    
+    return 0;
+}
