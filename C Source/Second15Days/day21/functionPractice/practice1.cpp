9cd541f02a138d6826ec280e5bb4fa831582c7bf
farhan2743 <hfarhan2743@gmail.com>
2022-11-21 21:27:22 +0600

    da

---
+++ C Source/Second15Days/day21/functionPractice/practice1.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void printline(void){
+    int i;
+    for ( i = 0; i < 40; i++)
+    {
+        printf("_");
+    }
+        printf("\n");
+
+}    
+    int main(){
+        printline();
+        printf("My name is MD. Farhanul Islam\n");
+        printline(); 
+    }
---
+++ C Source/Second15Days/day21/functionPractice/practice2.cpp
@@ -0,0 +1,10 @@
+/*Write a C program to reverse the digits of a given integer. Go to the editor
+Example:
+Input:
+i = 123
+i = 208478933
+i = -73634
+Output:
+Reverse integer: 321
+Reverse integer: 339874802
+Reverse integer: -43637*/
