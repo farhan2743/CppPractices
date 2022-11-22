50800d366b53619f4e405897b49375d8323307ae
farhan2743 <hfarhan2743@gmail.com>
2022-11-22 15:31:30 +0600

    hg

---
+++ C Source/Second15Days/day21/problemPractice/practice3.cpp
@@ -0,0 +1,22 @@
+/*
+2. Write a C program to check whether an integer is a palindrome or not. An integer is a palindrome when it reads the
+ same forward as backward. Go to the editor
+Example:
+Input:
+i = 1221
+i = -121
+i = 100
+Output:
+Is Palindrome: 1
+Is Palindrome: 0
+Is Palindrome: 0
+*/
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int num, i, isPellindrome;
+    printf("Enter any number : ");
+    scanf("%d",&num);
+
+    return 0;
+}
