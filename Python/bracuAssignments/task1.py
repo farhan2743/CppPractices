548b5076b9aa2c2a5e16909a36352f050bc28a40
farhan2743 <hfarhan2743@gmail.com>
2023-01-30 20:47:56 +0600

    1

---
+++ Python/bracuAssignments/task1.py
@@ -0,0 +1,14 @@
+
+var_1 = input('Please enter the first number: ')
+
+var_2 = input('Please enter the second number: ')
+print(type(var_1))
+print(type(var_2))
+var_3 = int(var_1)
+var_4 = int(var_2)
+total = var_3 + var_4
+product = var_3 * var_4
+difference = var_3 - var_4
+print("Sum =", total)
+print("Product =", product)
+print("Difference =", difference)
